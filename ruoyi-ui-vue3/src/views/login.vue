aa1314e7d7269c400da2e74697043663e693d3db
lanjian <lanjian@oortcloudsmart.com>
2026-08-06 17:06:36 +0800

    fix: 修复跳转登录的问题

--- ruoyi-ui-vue3/src/views/login.vue
+++ ruoyi-ui-vue3/src/views/login.vue
@@ -1,121 +1,120 @@
 <template>
   <div class="login">
     <div class="login-box">
-      <el-form ref="loginRef" :model="loginForm" :rules="loginRules" class="login-form">
-        <h3 class="title">{{ title }}</h3>
-        <el-form-item v-if="giteeStar === 'true'">
-          <el-text class="mx-1">给本项目star后即可访问：</el-text><el-link type="primary" href="https://gitee.com/xiaochemgzi/RuoYi-Wvp" target="_blank">点我去star</el-link>
-        </el-form-item>
-        <el-form-item prop="username">
-          <el-input
-              v-model="loginForm.username"
-              type="text"
-              size="large"
-              auto-complete="off"
-              placeholder="账号"
-          >
-            <template #prefix>
-              <svg-icon icon-class="user" class="el-input__icon input-icon"/>
-            </template>
-          </el-input>
-        </el-form-item>
-        <el-form-item prop="password">
-          <el-input
-              v-model="loginForm.password"
-              type="password"
-              size="large"
-              auto-complete="off"
-              placeholder="密码"
-              @keyup.enter="handleLogin"
-          >
-            <template #prefix>
-              <svg-icon icon-class="password" class="el-input__icon input-icon"/>
-            </template>
-          </el-input>
-        </el-form-item>
-        <el-form-item prop="code" v-if="captchaEnabled">
-          <el-input
-              v-model="loginForm.code"
-              size="large"
-              auto-complete="off"
-              placeholder="验证码"
-              style="width: 63%"
-              @keyup.enter="handleLogin"
-          >
-            <template #prefix>
-              <svg-icon icon-class="validCode" class="el-input__icon input-icon"/>
-            </template>
-          </el-input>
-          <div class="login-code">
-            <img :src="codeUrl" @click="getCode" class="login-code-img"/>
-          </div>
-        </el-form-item>
-        <el-checkbox v-model="loginForm.rememberMe" style="margin:0px 0px 25px 0px;">记住密码</el-checkbox>
-        <el-form-item style="width:100%;">
-          <el-button
-              :loading="loading"
-              size="large"
-              type="primary"
-              style="width:100%;"
-              @click.prevent="handleLogin"
-          >
-            <span v-if="!loading">登 录</span>
-            <span v-else>登 录 中...</span>
-          </el-button>
-          <div style="float: right;" v-if="register">
-            <router-link class="link-type" :to="'/register'">立即注册</router-link>
-          </div>
-        </el-form-item>
-      </el-form>
+<!--      <el-form ref="loginRef" :model="loginForm" :rules="loginRules" class="login-form">-->
+<!--        <h3 class="title">{{ title }}</h3>-->
+<!--        <el-form-item v-if="giteeStar === 'true'">-->
+<!--          <el-text class="mx-1">给本项目star后即可访问：</el-text><el-link type="primary" href="https://gitee.com/xiaochemgzi/RuoYi-Wvp" target="_blank">点我去star</el-link>-->
+<!--        </el-form-item>-->
+<!--        <el-form-item prop="username">-->
+<!--          <el-input-->
+<!--              v-model="loginForm.username"-->
+<!--              type="text"-->
+<!--              size="large"-->
+<!--              auto-complete="off"-->
+<!--              placeholder="账号"-->
+<!--          >-->
+<!--            <template #prefix>-->
+<!--              <svg-icon icon-class="user" class="el-input__icon input-icon"/>-->
+<!--            </template>-->
+<!--          </el-input>-->
+<!--        </el-form-item>-->
+<!--        <el-form-item prop="password">-->
+<!--          <el-input-->
+<!--              v-model="loginForm.password"-->
+<!--              type="password"-->
+<!--              size="large"-->
+<!--              auto-complete="off"-->
+<!--              placeholder="密码"-->
+<!--              @keyup.enter="handleLogin"-->
+<!--          >-->
+<!--            <template #prefix>-->
+<!--              <svg-icon icon-class="password" class="el-input__icon input-icon"/>-->
+<!--            </template>-->
+<!--          </el-input>-->
+<!--        </el-form-item>-->
+<!--        <el-form-item prop="code" v-if="captchaEnabled">-->
+<!--          <el-input-->
+<!--              v-model="loginForm.code"-->
+<!--              size="large"-->
+<!--              auto-complete="off"-->
+<!--              placeholder="验证码"-->
+<!--              style="width: 63%"-->
+<!--              @keyup.enter="handleLogin"-->
+<!--          >-->
+<!--            <template #prefix>-->
+<!--              <svg-icon icon-class="validCode" class="el-input__icon input-icon"/>-->
+<!--            </template>-->
+<!--          </el-input>-->
+<!--          <div class="login-code">-->
+<!--            <img :src="codeUrl" @click="getCode" class="login-code-img"/>-->
+<!--          </div>-->
+<!--        </el-form-item>-->
+<!--        <el-checkbox v-model="loginForm.rememberMe" style="margin:0px 0px 25px 0px;">记住密码</el-checkbox>-->
+<!--        <el-form-item style="width:100%;">-->
+<!--          <el-button-->
+<!--              :loading="loading"-->
+<!--              size="large"-->
+<!--              type="primary"-->
+<!--              style="width:100%;"-->
+<!--              @click.prevent="handleLogin"-->
+<!--          >-->
+<!--            <span v-if="!loading">登 录</span>-->
+<!--            <span v-else>登 录 中...</span>-->
+<!--          </el-button>-->
+<!--          <div style="float: right;" v-if="register">-->
+<!--            <router-link class="link-type" :to="'/register'">立即注册</router-link>-->
+<!--          </div>-->
+<!--        </el-form-item>-->
+<!--      </el-form>-->
       <div class="el-login-footer">
-        <span>Copyright © 2024-2025 ry-wvp.xyz All Rights Reserved.</span>
+        <span>Copyright © 2024-2025 视频监控平台 All Rights Reserved.</span>
       </div>
     </div>
     <!--  底部  -->
 
-    <el-dialog title="关注公众号" v-model="open" width="500px" append-to-body>
-      <el-form ref="loginFormRef" :model="loginForm" :rules="loginRules" label-width="120px">
-        <el-form-item label="公众号二维码">
-          <el-image style="width: 150px" :src="gzhImage" :preview-src-list="[gzhImage]"></el-image>
-        </el-form-item>
-        <el-form-item label="公众号code" prop="publicCode">
-          <template #label>
-            <span>
-               <el-tooltip
-                   content="关注公众号获取"
-                   placement="top">
-                  <el-icon><question-filled/></el-icon>
-               </el-tooltip>
-               公众号code
-            </span>
-          </template>
-          <el-input
-              v-model="loginForm.publicCode"
-              type="text"
-              size="large"
-              placeholder="请输入公众号code"
-          >
-          </el-input>
-        </el-form-item>
-      </el-form>
-      <template #footer>
-        <div class="dialog-footer">
-          <el-button type="primary" @click="submitForm">确 定</el-button>
-          <el-button @click="open = false">取 消</el-button>
-        </div>
-      </template>
-    </el-dialog>
+<!--    <el-dialog title="关注公众号" v-model="open" width="500px" append-to-body>-->
+<!--      <el-form ref="loginFormRef" :model="loginForm" :rules="loginRules" label-width="120px">-->
+<!--        <el-form-item label="公众号二维码">-->
+<!--          <el-image style="width: 150px" :src="gzhImage" :preview-src-list="[gzhImage]"></el-image>-->
+<!--        </el-form-item>-->
+<!--        <el-form-item label="公众号code" prop="publicCode">-->
+<!--          <template #label>-->
+<!--            <span>-->
+<!--               <el-tooltip-->
+<!--                   content="关注公众号获取"-->
+<!--                   placement="top">-->
+<!--                  <el-icon><question-filled/></el-icon>-->
+<!--               </el-tooltip>-->
+<!--               公众号code-->
+<!--            </span>-->
+<!--          </template>-->
+<!--          <el-input-->
+<!--              v-model="loginForm.publicCode"-->
+<!--              type="text"-->
+<!--              size="large"-->
+<!--              placeholder="请输入公众号code"-->
+<!--          >-->
+<!--          </el-input>-->
+<!--        </el-form-item>-->
+<!--      </el-form>-->
+<!--      <template #footer>-->
+<!--        <div class="dialog-footer">-->
+<!--          <el-button type="primary" @click="submitForm">确 定</el-button>-->
+<!--          <el-button @click="open = false">取 消</el-button>-->
+<!--        </div>-->
+<!--      </template>-->
+<!--    </el-dialog>-->
   </div>
 </template>
 
 <script setup>
-import {getCodeImg} from "@/api/login";
-import Cookies from "js-cookie";
-import {decrypt, encrypt} from "@/utils/jsencrypt";
 import useUserStore from '@/store/modules/user'
-import {getConfigKey} from "../api/system/config.js";
-import {giteeLogin} from "../api/login.js";
-import {deleteDevice} from "../api/wvp/device.js";
+import {isHttp} from "@/utils/validate.js";
+import {isRelogin} from "@/utils/request.js";
+import {setToken} from "@/utils/auth.js";
+import usePermissionStore from "@/store/modules/permission.js";
+import {useRoute, useRouter} from "vue-router";
 
 const title = import.meta.env.VITE_APP_TITLE;
 const userStore = useUserStore();
@@ -124,84 +123,27 @@ const router = useRouter();
 const {proxy} = getCurrentInstance();
 
 const loginForm = ref({
-  username: "",
-  password: "",
+  username: "ry",
+  password: "123456",
   rememberMe: false,
   code: "",
   uuid: "",
   publicCode: ""
 });
 
-const loginRules = {
-  username: [{required: true, trigger: "blur", message: "请输入您的账号"}],
-  password: [{required: true, trigger: "blur", message: "请输入您的密码"}],
-  code: [{required: true, trigger: "change", message: "请输入验证码"}],
-  publicCode: [{required: true, trigger: "change", message: "请输入公众号code"}]
-};
-
 const codeUrl = ref("");
 const loading = ref(false);
 // 验证码开关
 const captchaEnabled = ref(true);
 // 注册开关
-const register = ref(false);
 const redirect = ref(undefined);
-const publicDemonstrate = ref("false");
-const giteeStar = ref("false");
-
-const open = ref(false);
-const gzhImage = ref("https://gdhxkj.oss-cn-guangzhou.aliyuncs.com/2025/04/21/834e25b18791464eb903ffc39fa9bd0c.jpg");
 
 watch(route, (newRoute) => {
   redirect.value = newRoute.query && newRoute.query.redirect;
 }, {immediate: true});
 
-function handleLogin() {
-  proxy.$refs.loginRef.validate(valid => {
-    if (valid) {
-      if (publicDemonstrate.value === 'true' && loginForm.value.username !== 'admin') {
-        open.value = true
-        return
-      }
-      if (giteeStar.value === 'true' && loginForm.value.username !== 'admin') {
-        proxy.$modal.confirm("访问本系统需检测是否star本项目，是否继续？").then(() => {
-          // 勾选了需要记住密码设置在 cookie 中设置记住用户名和密码
-          if (loginForm.value.rememberMe) {
-            Cookies.set("username", loginForm.value.username, {expires: 30});
-            Cookies.set("password", encrypt(loginForm.value.password), {expires: 30});
-            Cookies.set("rememberMe", loginForm.value.rememberMe, {expires: 30});
-          } else {
-            // 否则移除
-            Cookies.remove("username");
-            Cookies.remove("password");
-            Cookies.remove("rememberMe");
-          }
-          giteeLogin().then((res)=>{
-            window.location.href = res.msg
-          })
-        }).catch(() => {
-        });
-
-        return
-      }
-      login()
-    }
-  });
-}
-
 function login() {
   loading.value = true;
-  // 勾选了需要记住密码设置在 cookie 中设置记住用户名和密码
-  if (loginForm.value.rememberMe) {
-    Cookies.set("username", loginForm.value.username, {expires: 30});
-    Cookies.set("password", encrypt(loginForm.value.password), {expires: 30});
-    Cookies.set("rememberMe", loginForm.value.rememberMe, {expires: 30});
-  } else {
-    // 否则移除
-    Cookies.remove("username");
-    Cookies.remove("password");
-    Cookies.remove("rememberMe");
-  }
   // 调用action的登录方法
   userStore.login(loginForm.value).then(() => {
     const query = route.query;
@@ -211,69 +153,41 @@ function login() {
       }
       return acc;
     }, {});
-    router.push({path: redirect.value || "/", query: otherQueryParams});
+    router.push({path: redirect.value || "/", query: query});
   }).catch(() => {
     loading.value = false;
-    // 重新获取验证码
-    if (captchaEnabled.value) {
-      getCode();
-    }
   });
-}
-
-function getCode() {
-  getCodeImg().then(res => {
-    captchaEnabled.value = res.captchaEnabled === undefined ? true : res.captchaEnabled;
-    if (captchaEnabled.value) {
-      codeUrl.value = "data:image/gif;base64," + res.img;
-      loginForm.value.uuid = res.uuid;
-    }
-  });
-}
-
-function getCookie() {
-  const username = Cookies.get("username");
-  const password = Cookies.get("password");
-  const rememberMe = Cookies.get("rememberMe");
-  loginForm.value = {
-    username: username === undefined ? loginForm.value.username : username,
-    password: password === undefined ? loginForm.value.password : decrypt(password),
-    rememberMe: rememberMe === undefined ? false : Boolean(rememberMe)
-  };
-}
-
-function getConfigKeyFun() {
-  getConfigKey("sys_public_demonstrate").then(response => {
-    publicDemonstrate.value = response.msg
-  })
-
-  getConfigKey("sys_gitee_star").then(response => {
-    giteeStar.value = response.msg
-  })
 }
 
 function submitForm(){
   proxy.$refs.loginFormRef.validate(valid => {
     if(valid){
       login()
-      open.value = false
     }
   })
 }
 
 onMounted((()=>{
-  getCode();
-  getCookie();
-  getConfigKeyFun()
-
-  let giteeCode = route.query && route.query.code
-  if(giteeCode === "true"){
-    console.log("登录成功")
-    getCookie()
-    login()
-    proxy.$modal.msgSuccess("登录成功");
-  }else if(giteeCode === "false"){
-    proxy.$modal.msgError("登录失败:请先点star再登录");
+  const accessToken = new URL(window.location.href).searchParams.get("accessToken");
+  if (accessToken) {
+    setToken(accessToken);
+    console.log(accessToken);
+    useUserStore()
+        .getInfo()
+        .then(() => {
+          isRelogin.show = false;
+          usePermissionStore()
+              .generateRoutes()
+              .then((accessRoutes) => {
+                accessRoutes.forEach((route) => {
+                  if (!isHttp(route.path)) {
+                    router.addRoute(route);
+                  }
+                });
+                next({ ...to, replace: true });
+              });
+        })
+    router.push({ path: "/index" }).catch(() => { });
   }
 }))
 </script>
